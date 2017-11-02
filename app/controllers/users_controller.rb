ed496807f0157437c7b7b2029beda8908e23ea40
martink-io <martinkiogit@gmail.com>
2017-11-02 20:39:26 +0000

    Change number of articles on user's page to 5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,7 +36,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user_articles = @user.articles.paginate(page: params[:page], per_page: 2)
+    @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def destroy
