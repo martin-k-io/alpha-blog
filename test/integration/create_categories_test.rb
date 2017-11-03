b50b4bfdfa86d74299928731903b0a3ac6ceae93
martink-io <martinkiogit@gmail.com>
2017-11-03 10:26:25 +0000

    Add article and category association

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -8,7 +8,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
   end
 
   test "get new category form and create category" do
-    sign_in_as(@user, "password")
+    #sign_in_as(@user, "password")
     get new_category_path #user goes to the new category path
     assert_template 'categories/new' #user gets the new form
     assert_difference 'Category.count', 1 do #user posts to the new form
@@ -20,7 +20,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
   end
 
   test "invalid category submission results in failure" do
-    sign_in_as(@user, "password")
+    #sign_in_as(@user, "password")
     get new_category_path 
     assert_template 'categories/new' 
     assert_no_difference 'Category.count' do 
